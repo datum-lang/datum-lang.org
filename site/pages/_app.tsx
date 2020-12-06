fae9e6c2697388c99058d2159d3945103f1d1f4a
Elliot <hack00mind@gmail.com>
2020-12-06 15:16:31 +0800

    feat: support for tailwindcss, update index page

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,11 +1,15 @@
-import Header from '../components/Header'
+import Head from 'next/head'
+import { ReactElement } from 'react'
+import '../styles/globals.css'
 
-function MyApp ({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }): ReactElement {
   return (
-    <>
-      <Header />
+    <div className='h-screen'>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <Component {...pageProps} />
-    </>
+    </div>
   )
 }
 
--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,15 +1,28 @@
 import Head from 'next/head'
 import { FunctionComponent } from 'react'
+import Header from '../components/Header'
 
 const Home: FunctionComponent = () => {
   return (
-    <main>
+    <>
       <Head>
         <title>Charj</title>
-        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <p>Welcome to Charj</p>
-    </main>
+      <div className='bg-white'>
+        <div className='bg-gray-50 border-b border-gray-200'>
+          <Header />
+          <div className='max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 pt-12 pb-20 flex flex-col items-center'>
+            <h1 className='font-extrabold text-5xl leading-10 tracking-tight text-gray-900'>
+              Charj
+            </h1>
+            <h2 className='mt-4 sm:mt-5 font-light text-2xl text-center leading-tight text-gray-900'>
+              Charj Lang 是一个使用<strong className='font-semibold'>Rust</strong>编写的<strong className='font-semibold'>描述式、中间</strong>编程语言
+            </h2>
+
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
